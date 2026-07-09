8d1938025e202506bb477449e75cf6fbfd477501
laraib1915 <laraibfatimaawan@gmail.com>
2026-07-09 11:10:02 +0500

    vercel.0.3

--- frontend/src/api/api.jsx
+++ frontend/src/api/api.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Use 127.0.0.1 instead of localhost
 const API_BASE_URL =
-  import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000/api';
+  import.meta.env.VITE_API_URL || "";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
